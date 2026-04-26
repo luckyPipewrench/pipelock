0d30b88a94130b3a8a7772759c38459ddb79fcda
luckyPipewrench <gitlucky@pipelab.org>
2026-04-26 00:50:51 -0400

    feat(contract): bootstrap internal/contract package

---
+++ internal/contract/contract_test.go
@@ -0,0 +1,11 @@
+// Copyright 2026 Josh Waldrep
+// SPDX-License-Identifier: Apache-2.0
+
+package contract
+
+import "testing"
+
+func TestPackageBootstraps(t *testing.T) {
+	t.Parallel()
+	// Smoke test: package compiles. More tests added as types land.
+}

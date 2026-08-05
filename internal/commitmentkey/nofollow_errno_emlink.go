440a602e79c209e03ee60bb55aec5d43144aeac9
luckyPipewrench <gitlucky@pipelab.org>
2026-08-05 16:00:24 -0400

    fix(commitment-key): treat DragonFly like FreeBSD for O_NOFOLLOW symlink errno

--- internal/commitmentkey/nofollow_errno_emlink.go
+++ internal/commitmentkey/nofollow_errno_emlink.go
@@ -1,11 +1,11 @@
 // Copyright 2026 Josh Waldrep
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build freebsd
+//go:build freebsd || dragonfly
 
 package commitmentkey
 
 import "golang.org/x/sys/unix"
 
 // FreeBSD reports EMLINK when O_NOFOLLOW refuses a final-component symlink.
-var noFollowSymlinkErrors = noFollowSymlinkErrorsFor("freebsd", noFollowErrnos{loop: unix.ELOOP, link: unix.EMLINK})
+var noFollowSymlinkErrors = noFollowSymlinkErrorsFor("freebsd-family", noFollowErrnos{loop: unix.ELOOP, link: unix.EMLINK})
--- internal/commitmentkey/nofollow_errno_unix.go
+++ internal/commitmentkey/nofollow_errno_unix.go
@@ -1,7 +1,7 @@
 // Copyright 2026 Josh Waldrep
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build unix && !freebsd && !netbsd
+//go:build unix && !freebsd && !dragonfly && !netbsd
 
 package commitmentkey
 
